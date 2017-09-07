ee51b679391ebfa1c199bf7647465559eb909307
zyrill <zyrill@users.noreply.github.com>
2017-09-07 16:02:14 +0200

    Create HeiseAdBlock.user.js

---
+++ HeiseAdBlock.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name        Heise Adblocker
+// @namespace   https://github.com/zyrill/HeiseAdBlock
+// @description Adjusts design of Heise when using Adblock Plus
+// @author      zyrill
+// @include     https://www.heise.de/*
+// @encoding    utf-8
+// @description Remove Golem Ads
+// @grant       none
+// @run-at      document-idle
+// @version     1.0
+// ==/UserScript==
+
+document.getElementById("container_content").style.top="0px";
+document.getElementById("mitte_links").style.width="100%";
