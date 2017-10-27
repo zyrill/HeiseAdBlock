0f3d1a0c4ef3c581bc7bc9147da2f40b3bf770fc
zyrill <zyrill@users.noreply.github.com>
2017-10-27 23:51:01 +0200

    Update HeiseAdBlock.user.js

--- HeiseAdBlock.user.js
+++ HeiseAdBlock.user.js
@@ -6,10 +6,10 @@
 // @include     https://www.heise.de/*
 // @include     http://www.heise.de/*
 // @encoding    utf-8
-// @description Remove Golem Ads
+// @description Remove some leftover ads that the adblocker doesn't catch
 // @grant       none
 // @run-at      document-idle
-// @version     1.0.1
+// @version     1.0.2
 // ==/UserScript==
 
 document.getElementById("container_content").style.top="0px";
